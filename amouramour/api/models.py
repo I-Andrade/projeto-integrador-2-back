66fca29bdd1cff817d84f6a3981ab6834ceef26b
Tairone Amaral <2006878@aluno.univesp.br>
2022-05-14 14:21:11 -0300

    correcao models

--- amouramour/api/models.py
+++ amouramour/api/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Clientes(models.Model):
     id = models.AutoField(primary_key=True)
-    tipo = models.IntegerField(max_length=3)
+    tipo = models.IntegerField()
     nome = models.CharField(max_length=254)
     cpf_cnpj = models.CharField(max_length=14)
     data_nascimento = models.DateField()
@@ -22,13 +22,16 @@ class Clientes(models.Model):
 class Transportadoras(models.Model):
     id = models.AutoField(primary_key=True)
     nome = models.CharField(max_length=254)
+
 class FormaPagto(models.Model):
     id = models.AutoField(primary_key=True)
     descricao = models.CharField(max_length=60)   
-    parcelas = models.IntegerField(max_length=2)
+    parcelas = models.IntegerField()
+
 class Status(models.Model):
     id = models.AutoField(primary_key=True)
-    descricao = models.CharField(max_length=60)     
+    descricao = models.CharField(max_length=60) 
+
 class Pedido(models.Model):
     id = models.AutoField(primary_key=True)
     cliente_id = models.ForeignKey(Clientes, on_delete=models.CASCADE)
@@ -36,29 +39,30 @@ class Pedido(models.Model):
     data_entrega = models.DateField()
     obs = models.TextField(max_length=255)  
     status = models.CharField(max_length=10)
-    valor_total_bruto = models.FloatField(max_digits=10, decimal_places=2) 
-    valor_frete = models.FloatField(max_digits=10, decimal_places=2) 
-    desconto = models.FloatField(max_digits=10, decimal_places=2)
-    valor_total_liquido = models.FloatField(max_digits=10, decimal_places=2)
+    valor_total_bruto = models.FloatField(max_length=10) 
+    valor_frete = models.FloatField(max_length=10) 
+    desconto = models.FloatField(max_length=10)
+    valor_total_liquido = models.FloatField(max_length=10)
     transportadora_id = models.ForeignKey(Transportadoras, on_delete=models.CASCADE)
     forma_pagto_id = models.ForeignKey(FormaPagto, on_delete=models.CASCADE)
     status_id = models.ForeignKey(Status, on_delete=models.CASCADE)
+
 class Produto(models.Model):
     id = models.AutoField(primary_key=True)
     descricao = models.CharField(max_length=100)
     tamanho = models.CharField(max_length=50)
     cor = models.CharField(max_length=50)
-    valor_custo = models.FloatField(max_digits=10, decimal_places=2)
-    valor_venda = models.FloatField(max_digits=10, decimal_places=2)
+    valor_custo = models.FloatField(max_length=10)
+    valor_venda = models.FloatField(max_length=10)
     obs = models.TextField(max_length=255)
+
 class Item_pedido(models.Model):
-    produto_id = models.ForeignKey(Produto, on_delete=models.CASCADE)
-    valor_custo = models.FloatField(max_digits=10, decimal_places=2)
-    valor_venda = models.FloatField(max_digits=10, decimal_places=2)
-    quantidade = models.IntegerField(max_length=3)
+    id = models.AutoField(primary_key=True)
+    valor_custo = models.FloatField(max_length=10)
+    valor_venda = models.FloatField(max_length=10)
+    quantidade = models.IntegerField()
     nome_crianca = models.CharField(max_length=100)   
-    dt_nasc_crianca = models.DateField(Required=False)
+    dt_nasc_crianca = models.DateField()
     medidas = models.CharField(max_length=200)
     sexo = models.CharField(max_length=12) 
-
 

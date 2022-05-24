66c726b22f97931917b013206891f027e8a2dbfe
I.Andrade <irfley@hotmail.com>
2022-05-23 22:16:23 -0300

    Devolvendo token para developer

--- amouramour/settings.py
+++ amouramour/settings.py
@@ -49,12 +49,12 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
-    #'DEFAULT_AUTHENTICATION_CLASSES': (
-	#	'rest_framework_simplejwt.authentication.JWTAuthentication',
-	#),
-    #'DEFAULT_PERMISSION_CLASSES': (
-	#	'rest_framework.permissions.IsAuthenticated',
-	#)
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+		'rest_framework_simplejwt.authentication.JWTAuthentication',
+	),
+    'DEFAULT_PERMISSION_CLASSES': (
+		'rest_framework.permissions.IsAuthenticated',
+	)
 }
 
 MIDDLEWARE = [
